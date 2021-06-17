dc19af840bc3ee2dfa5d9d8fe3aa6ed46cdf473d
typowritter <yxnan@pm.me>
2021-06-18 03:11:35 +0800

    use rgb888

--- Core/Inc/lcd.h
+++ Core/Inc/lcd.h
@@ -23,38 +23,39 @@ extern "C" {
 #define DMA2D_COLOR_MODE   DMA2D_OUTPUT_RGB565
 #define MAX_LAYERS         1 /* one layer */
 
-typedef uint16_t color_t;    /* RGB565 */
+/* we use RGB888 internally, DMA2D will do the conversion on output */
+typedef uint32_t color_t;
+typedef uint32_t vmem_addr_t; /* video memory address */
 
 /* END project specific setups */
 
-
-enum rgb565_enum {
-  aqua       = 0x07ff,
-  black      = 0x0000,
-  blue       = 0x001f,
-  cyan       = 0x07ff,
-  darkcyan   = 0x03ef,
-  darkgreen  = 0x03e0,
-  darkgrey   = 0x7bef,
-  fuchsia    = 0xf81f,
-  gray1      = 0x8410,
-  gray2      = 0x4208,
-  gray3      = 0x2104,
-  green      = 0x07e0,
-  lightgreen = 0xafe5,
-  lightgrey  = 0xc618,
-  lime       = 0x07e0,
-  magenta    = 0xf81f,
-  maroon     = 0x7800,
-  navy       = 0x000f,
-  olive      = 0x8400,
-  orange     = 0xfd20,
-  purple     = 0x780f,
-  red        = 0xf800,
-  silver     = 0xc618,
-  teal       = 0x0410,
-  white      = 0xffff,
-  yellow     = 0xffe0,
+enum rgb888_enum {
+  aqua       = 0x00ffff,
+  black      = 0x000000,
+  blue       = 0x0000ff,
+  cyan       = 0x00ffff,
+  darkcyan   = 0x007d7b,
+  darkgreen  = 0x007d00,
+  darkgrey   = 0x7b7d7b,
+  fuchsia    = 0xff00ff,
+  gray1      = 0x848284,
+  gray2      = 0x424142,
+  gray3      = 0x212021,
+  green      = 0x00ff00,
+  lightgreen = 0xadff29,
+  lightgrey  = 0xc5c2c5,
+  lime       = 0x00ff00,
+  magenta    = 0xff00ff,
+  maroon     = 0x7b0000,
+  navy       = 0x00007b,
+  olive      = 0x848200,
+  orange     = 0xffa600,
+  purple     = 0x7b007b,
+  red        = 0xff0000,
+  silver     = 0xc5c2c5,
+  teal       = 0x008284,
+  white      = 0xffffff,
+  yellow     = 0xffff00,
 };
 
 typedef struct
