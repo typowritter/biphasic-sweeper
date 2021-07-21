908ef6bbeee0dbf4a2065af7c3867bc0f17c2412
typowritter <yxnan@pm.me>
2021-07-21 23:31:23 +0800

    cmd_queue

--- Core/Inc/tft/cmd_queue.h
+++ Core/Inc/tft/cmd_queue.h
@@ -1,36 +1,44 @@
-/*!
- *  \file cmd_queue.h
- *  \brief 串口指令队列
- *  \version 1.0
- *  \date 2012-2015
- *  \copyright 广州大彩光电科技有限公司
- */
+/**
+  ******************************************************************************
+  * @file    cmd_queue.h
+  * @author  yxnan <yxnan@pm.me>
+  * @date    2021-07-21
+  * @brief   流指令队列实现
+  ******************************************************************************
+  */
 
-#ifndef _CMD_QUEUE
-#define _CMD_QUEUE
-#include "tft/hmi_driver.h"
+#ifndef __CMD_QUEUE
+#define __CMD_QUEUE
 
-typedef unsigned char qdata;
-typedef unsigned short qsize;
-
-/*!
- *  \brief  清空指令数据
- */
-extern void queue_reset(void);
-
-/*!
- * \brief  添加指令数据
- * \detial 串口接收的数据，通过此函数放入指令队列
- *  \param  _data 指令数据
- */
-extern void queue_push(qdata _data);
-
-/*!
- *  \brief  从指令队列中取出一条完整的指令
- *  \param  cmd 指令接收缓存区
- *  \param  buf_len 指令接收缓存区大小
- *  \return  指令长度，0表示队列中无完整指令
- */
-extern qsize queue_find_cmd(qdata *cmd,qsize buf_len);
-
+#ifdef  __cplusplus
+extern "C" {
 #endif
+
+#include <stdint.h>
+
+typedef uint8_t  qdata_t;
+typedef uint16_t qsize_t;
+
+/**
+ * 清空指令数据
+ */
+void queue_reset(void);
+
+/**
+ * UART接收的数据，通过此函数放入指令队列
+ */
+void queue_push(qdata_t data);
+
+/**
+ * 从指令队列中取出一条完整的指令
+ *
+ * @param cmd       -- 指令接收缓存区
+ * @param buf_len   -- 指令接收缓存区大小
+ * @return          -- 指令长度，0表示队列中无完整指令
+ */
+qsize_t queue_extract_cmd(qdata_t *cmd, qsize_t buf_len);
+
+#ifdef  __cplusplus
+}
+#endif
+#endif /* __CMD_QUEUE */
--- Core/Src/tft/cmd_queue.c
+++ Core/Src/tft/cmd_queue.c
@@ -1,100 +1,125 @@
+/**
+  ******************************************************************************
+  * @file    cmd_queue.c
+  * @author  yxnan <yxnan@pm.me>
+  * @date    2021-07-21
+  * @brief   流指令队列实现
+  ******************************************************************************
+  */
+
+#include "tft/conf.h"
 #include "tft/cmd_queue.h"
 
-#define CMD_HEAD 0XEE                                                  //帧头
-#define CMD_TAIL 0XFFFCFFFF                                            //帧尾
+#define CMD_HEAD 0XEE
+#define CMD_TAIL 0XFFFCFFFF
 
-typedef struct _QUEUE
+static void queue_pop(qdata_t *data);
+static qsize_t queue_size();
+
+typedef struct
 {
-    qsize _head;                                                       //队列头
-    qsize _tail;                                                       //队列尾
-    qdata _data[QUEUE_MAX_SIZE];                                       //队列数据缓存区
-}QUEUE;
+  qsize_t head;
+  qsize_t tail;
+  qdata_t data[QUEUE_MAX_SIZE];
+} queue_t;
 
-static QUEUE que = {0,0,{0}};                                            //指令队列
-static uint32 cmd_state = 0;                                           //队列帧尾检测状态
-static qsize cmd_pos = 0;                                              //当前指令指针位置
+static queue_t g_queue = {0, 0, {0}};
+static int g_cmd_count = 0;
 
-/*!
-*  \brief  清空指令数据
-*/
+/**
+ * 清空指令数据
+ */
 void queue_reset()
 {
-    que._head = que._tail = 0;
-    cmd_pos = cmd_state = 0;
+  g_queue.head = g_queue.tail = 0;
+  g_cmd_count = 0;
 }
-/*!
-* \brief  添加指令数据
-* \detial 串口接收的数据，通过此函数放入指令队列
-*  \param  _data 指令数据
-*/
-void queue_push(qdata _data)
+
+/**
+ * UART接收的数据，通过此函数放入指令队列
+ */
+void queue_push(qdata_t data)
 {
-    qsize pos = (que._head+1)%QUEUE_MAX_SIZE;
-    if(pos!=que._tail)                                                //非满状态
+  static uint32_t cmd_state = 0;
+  qsize_t pos = (g_queue.head + 1) % QUEUE_MAX_SIZE;
+
+  if(pos != g_queue.tail)       /* 非满状态 */
+  {
+    g_queue.data[g_queue.head] = data;
+    g_queue.head = pos;
+  }
+
+  cmd_state = (cmd_state<<8) | data;
+  if (cmd_state == CMD_TAIL)    /* 检测到指令尾 */
+  {
+    cmd_state = 0;
+    g_cmd_count++;
+  }
+}
+
+/**
+ * 从队列中取出一个数据
+ */
+static void queue_pop(qdata_t *data)
+{
+  if (g_queue.tail != g_queue.head)  /* 非空状态 */
+  {
+    *data = g_queue.data[g_queue.tail];
+    g_queue.tail = (g_queue.tail + 1) % QUEUE_MAX_SIZE;
+  }
+}
+
+static qsize_t queue_size()
+{
+  return (g_queue.head + QUEUE_MAX_SIZE - g_queue.tail) % QUEUE_MAX_SIZE;
+}
+
+/**
+ * 从指令队列中取出一条完整的指令
+ *
+ * @param cmd       -- 指令接收缓存区
+ * @param buf_len   -- 指令接收缓存区大小
+ * @return          -- 指令长度，0表示队列中无完整指令
+ */
+qsize_t queue_extract_cmd(qdata_t *buffer, qsize_t buf_len)
+{
+  if (g_cmd_count == 0)
+    return 0;
+
+  static uint32_t cmd_state = 0;
+  qsize_t cmd_pos = 0;
+  qsize_t cmd_size = 0;
+  qdata_t data = 0;
+
+  while (queue_size() > 0)
+  {
+    queue_pop(&data);
+
+    if (cmd_pos==0 && data!=CMD_HEAD)
+      continue;
+
+    if (cmd_pos < buf_len)              /* 防止缓冲区溢出 */
+      buffer[cmd_pos++] = data;
+
+    cmd_state = (cmd_state<<8) | data;  /* 拼接最后4个字节，组成一个32位整数 */
+
+    //最后4个字节与帧尾匹配，得到完整帧
+    if (cmd_state == CMD_TAIL)
     {
-        que._data[que._head] = _data;
-        que._head = pos;
-    }
-}
+      //LED2_ON;
+      cmd_size = cmd_pos;             /* 指令字节长度 */
+      cmd_state = 0;                  /* 重新检测帧尾巴 */
+      g_cmd_count--;
 
-//从队列中取一个数据
-static void queue_pop(qdata* _data)
-{
-    if(que._tail!=que._head)                                          //非空状态
-    {
-        *_data = que._data[que._tail];
-        que._tail = (que._tail+1)%QUEUE_MAX_SIZE;
-    }
-}
+#if (CRC16_ENABLE)
+      /* 去掉指令头尾EE，尾FFFCFFFF共计5个字节，只计算数据部分CRC */
+      if (!CheckCRC16(buffer+1, cmd_size-5))
+        return 0;
 
-//获取队列中有效数据个数
-static qsize queue_size()
-{
-    return ((que._head+QUEUE_MAX_SIZE-que._tail)%QUEUE_MAX_SIZE);
-}
-/*!
-*  \brief  从指令队列中取出一条完整的指令
-*  \param  cmd 指令接收缓存区
-*  \param  buf_len 指令接收缓存区大小
-*  \return  指令长度，0表示队列中无完整指令
-*/
-qsize queue_find_cmd(qdata *buffer,qsize buf_len)
-{
-    qsize cmd_size = 0;
-    qdata _data = 0;
-
-    while(queue_size()>0)
-    {
-        //取一个数据
-        queue_pop(&_data);
-
-        if(cmd_pos==0&&_data!=CMD_HEAD)                               //指令第一个字节必须是帧头，否则跳过
-        {
-            continue;
-        }
-        //    LED2_ON;
-        if(cmd_pos<buf_len)                                           //防止缓冲区溢出
-            buffer[cmd_pos++] = _data;
-
-        cmd_state = ((cmd_state<<8)|_data);                           //拼接最后4个字节，组成一个32位整数
-
-        //最后4个字节与帧尾匹配，得到完整帧
-        if(cmd_state==CMD_TAIL)
-        {
-            //LED2_ON;
-            cmd_size = cmd_pos;                                       //指令字节长度
-            cmd_state = 0;                                            //重新检测帧尾巴
-            cmd_pos = 0;                                              //复位指令指针
-
-#if(CRC16_ENABLE)
-            //去掉指令头尾EE，尾FFFCFFFF共计5个字节，只计算数据部分CRC
-            if(!CheckCRC16(buffer+1,cmd_size-5))                      //CRC校验
-                return 0;
-
-            cmd_size -= 2;                                            //去掉CRC16（2字节）
+      cmd_size -= 2;                  /* 去掉CRC16（2字节） */
 #endif
-            return cmd_size;
-        }
+      return cmd_size;
     }
-    return 0;                                                         //没有形成完整的一帧
+  }
+  return 0;                           /* 没有形成完整的一帧 */
 }
